06d7a199355bf8808c3e0f606c3a0d702983557a
Flavio S. Glock <fglock@gmail.com>
2010-01-05 15:49:56 +0100

    rm mp6-javascript.pl; use mp6.pl -Cjs instead

--- util-perl5/build-javascript.sh
+++ util-perl5/build-javascript.sh
@@ -1,4 +1,4 @@
-# Compile MiniPerl6 to Javascript, using mp6-javascript.pl
+# Compile MiniPerl6 to Javascript, using mp6.pl -Cjs
 
 #rm -rf libjs
 mkdir libjs
@@ -15,43 +15,43 @@ mkdir libjs/MiniPerl6/Lisp
 
 cp lib/MiniPerl6/Javascript/Runtime.js libjs/MiniPerl6/Javascript/
 
-perl mp6-javascript.pl \
+perl mp6.pl -Cjs \
     <    lib/MiniPerl6/Javascript/Prelude.pm    \
     >  libjs/MiniPerl6/Javascript/Prelude.js
 
-perl mp6-javascript.pl \
+perl mp6.pl -Cjs \
     <    lib/MiniPerl6/Javascript/Emitter.pm    \
     >  libjs/MiniPerl6/Javascript/Emitter.js
 
-perl mp6-javascript.pl \
+perl mp6.pl -Cjs \
     <    lib/Test.pm    \
     >  libjs/Test.js
 
-# perl mp6-javascript.pl \
+# perl mp6.pl -Cjs \
 #    <    lib/MiniPerl6/AST/CompUnit.pm    \
 #    >  libjs/MiniPerl6/AST/CompUnit.js
 
-perl mp6-javascript.pl \
+perl mp6.pl -Cjs \
     <    lib/MiniPerl6/Grammar.pm    \
     >  libjs/MiniPerl6/Grammar.js
 
-perl mp6-javascript.pl \
+perl mp6.pl -Cjs \
     <    lib/MiniPerl6/Grammar/Control.pm    \
     >  libjs/MiniPerl6/Grammar/Control.js
 
-perl mp6-javascript.pl \
+perl mp6.pl -Cjs \
     <    lib/MiniPerl6/Grammar/Mapping.pm    \
     >  libjs/MiniPerl6/Grammar/Mapping.js
 
-perl mp6-javascript.pl \
+perl mp6.pl -Cjs \
     <    lib/MiniPerl6/Grammar/Regex.pm    \
     >  libjs/MiniPerl6/Grammar/Regex.js
 
-perl mp6-javascript.pl \
+perl mp6.pl -Cjs \
     <    lib/MiniPerl6/Emitter/Token.pm    \
     >  libjs/MiniPerl6/Emitter/Token.js
 
-perl mp6-javascript.pl \
+perl mp6.pl -Cjs \
     <    lib/MiniPerl6/Perl5/Emitter.pm    \
     >  libjs/MiniPerl6/Perl5/Emitter.js
 
