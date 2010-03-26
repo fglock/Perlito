b2c6369070c85200336aaa9603855b6ae77bda89
Flavio S. Glock <fglock@gmail.com>
2010-03-26 14:05:24 +0100

    remove runjs.sh

--- runjs.sh
+++
@@ -1,9 +0,0 @@
-cat libjs/MiniPerl6/Javascript/Runtime.js \
-    libjs/MiniPerl6/Javascript/Prelude.js \
-    libjs/MiniPerl6/Javascript/Emitter.js \
-    libjs/MiniPerl6/Grammar.js \
-    libjs/MiniPerl6/Grammar/Control.js \
-    libjs/MiniPerl6/Grammar/Mapping.js \
-    libjs/MiniPerl6/Grammar/Regex.js \
-    main.js   \
-  > out.js
