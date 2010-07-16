4653e18c5f7c702466cd28004aca8e6e124a67fd
Flavio S. Glock <fglock@gmail.com>
2010-07-16 15:51:22 +0200

    Python backend - util-python/build-python.sh builds all modules

--- util-python/build-python.sh
+++ util-python/build-python.sh
@@ -12,11 +12,17 @@ touch libpy/miniperl6/python/__init__.py
 
 cp lib/MiniPerl6/Python/Runtime.py libpy/miniperl6/python/runtime.py
 
-perl mp6.pl -Cpython lib/Test.pm > libpy/Test.py
-perl mp6.pl -Cpython lib/MiniPerl6/Grammar.pm > libpy/MiniPerl6__Grammar.py
+perl mp6.pl -Cpython lib/Test.pm                    > libpy/Test.py
+perl mp6.pl -Cpython lib/MiniPerl6/Grammar.pm       > libpy/MiniPerl6__Grammar.py
 perl mp6.pl -Cpython lib/MiniPerl6/Grammar/Control.pm > libpy/MiniPerl6__Grammar__Control.py
 perl mp6.pl -Cpython lib/MiniPerl6/Grammar/Mapping.pm > libpy/MiniPerl6__Grammar__Mapping.py
 perl mp6.pl -Cpython lib/MiniPerl6/Grammar/Regex.pm > libpy/MiniPerl6__Grammar__Regex.py
 perl mp6.pl -Cpython lib/MiniPerl6/Emitter/Token.pm > libpy/MiniPerl6__Emitter__Token.py
-perl mp6.pl -Cpython lib/MiniPerl6/Eval.pm > libpy/MiniPerl6__Eval.py
+perl mp6.pl -Cpython lib/MiniPerl6/Eval.pm          > libpy/MiniPerl6__Eval.py
+
+perl mp6.pl -Cpython lib/MiniPerl6/Javascript/Emitter.pm > libpy/MiniPerl6__Javascript__Emitter.py
+perl mp6.pl -Cpython lib/MiniPerl6/Lisp/Emitter.pm  > libpy/MiniPerl6__Lisp__Emitter.py
+perl mp6.pl -Cpython lib/MiniPerl6/Perl5/Emitter.pm > libpy/MiniPerl6__Perl5__Emitter.py
+perl mp6.pl -Cpython lib/MiniPerl6/Go/Emitter.pm    > libpy/MiniPerl6__Go__Emitter.py
+perl mp6.pl -Cpython util-go/mp6-go-main.pm         > ./mp6.py
 
