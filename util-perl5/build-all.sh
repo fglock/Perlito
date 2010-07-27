6b5bb96156287f315d5562975d711d2c311c84e4
Flavio S. Glock <fglock@gmail.com>
2010-07-27 17:22:59 +0200

    util-perl5/build-all.sh - added Go binary to build

--- util-perl5/build-all.sh
+++ util-perl5/build-all.sh
@@ -3,6 +3,7 @@ perl util-perl5/make.pl -Cast-perl5 && rm -rf libast-perl5 && mv libast-perl5-ne
 perl util-perl5/make.pl -Cperl5 && rm -rf lib5 && mv lib5-new lib5
 perl util-perl5/make.pl -Cjs && rm -rf libjs && mv libjs-new libjs
 perl mp6.pl -Clisp-bin util/mp6.pl && mv tmp.out mp6-lisp
+perl mp6.pl -Cgo-bin util/mp6.pl && mv 6.out mp6-go
 . util-js/make-perlito-js.sh
 . util-python/build-python.sh 
 . util-ruby/build-ruby.sh 
