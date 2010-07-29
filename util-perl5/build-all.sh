4a80c27537b0391d8a8e9f468c39ca52c5a65d42
Flavio S. Glock <fglock@gmail.com>
2010-07-29 11:22:01 +0200

    build-all.sh - remove old binaries before recompiling

--- util-perl5/build-all.sh
+++ util-perl5/build-all.sh
@@ -1,10 +1,20 @@
 rm -rf libast-perl5
+
 perl util-perl5/make.pl -Cast-perl5 && rm -rf libast-perl5 && mv libast-perl5-new libast-perl5
+
 perl util-perl5/make.pl -Cperl5 && rm -rf lib5 && mv lib5-new lib5
+
 perl util-perl5/make.pl -Cjs && rm -rf libjs && mv libjs-new libjs
+
+rm tmp.out
 perl mp6.pl -Clisp-bin util/mp6.pl && mv tmp.out mp6-lisp
+
+rm 6.out
 perl mp6.pl -Cgo-bin util/mp6.pl && mv 6.out mp6-go
+
 . util-js/make-perlito-js.sh
+
 . util-python/build-python.sh 
+
 . util-ruby/build-ruby.sh 
 
