4a80a0ef8a7f3f9a63ce9c6e5ff067efffdacc15
Flavio S. Glock <fglock@gmail.com>
2011-11-15 17:07:47 +0100

    add Perlito::Runtime

---
+++ src/lib/Perlito/Runtime.pm
@@ -0,0 +1,27 @@
+class Main {
+
+    sub _replace($s, $old, $new) {
+        my $p = index($s, $old);
+        $p >= 0
+        ?? substr( $s, 0, $p ) ~ $new ~ _replace( substr( $s, $p + 1 ), $old, $new )
+        !! $s
+    }
+
+    method lisp_escape_string($s) {
+        _replace($s, "\\", "\\\\");
+    }
+
+    method to_javascript_namespace($s) {
+        _replace($s, "::", '$');
+    }
+
+    method to_lisp_namespace($s) {
+        _replace($s, "::", "-");
+    }
+
+    method to_go_namespace($s) {
+        _replace($s, "::", "__");
+    }
+
+}
+
