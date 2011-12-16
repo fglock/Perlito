8d0bfff1bbf49e4c7aadcbd5309f7b5b4e91ca4e
Flavio S. Glock <fglock@gmail.com>
2011-12-16 15:26:26 +0100

    Perlito5 - support less backends

--- util-perl5/bootstrap-perlito5-perl5.sh
+++ util-perl5/bootstrap-perlito5-perl5.sh
@@ -5,17 +5,12 @@ rm -rf lib5-new/Perlito5
 
 mkdir lib5-new
 mkdir lib5-new/Perlito5
-mkdir lib5-new/Perlito5/Clojure
 mkdir lib5-new/Perlito5/Emitter
-mkdir lib5-new/Perlito5/Go
 mkdir lib5-new/Perlito5/Grammar
 mkdir lib5-new/Perlito5/Java
 mkdir lib5-new/Perlito5/Javascript
-mkdir lib5-new/Perlito5/Lisp
-mkdir lib5-new/Perlito5/Parrot
 mkdir lib5-new/Perlito5/Perl5
 mkdir lib5-new/Perlito5/Python
-mkdir lib5-new/Perlito5/Rakudo
 mkdir lib5-new/Perlito5/Ruby
 
 cp src5/lib/Perlito5/Perl5/Runtime.pm lib5-new/Perlito5/Perl5/Runtime.pm
@@ -32,11 +27,8 @@ perl perlito5.pl -Cperl5 src5/lib/Perlito5/Grammar/Regex.pm   > lib5-new/Perlito
 perl perlito5.pl -Cperl5 src5/lib/Perlito5/Macro.pm           > lib5-new/Perlito5/Macro.pm
 perl perlito5.pl -Cperl5 src5/lib/Perlito5/Precedence.pm      > lib5-new/Perlito5/Precedence.pm
 
-perl perlito5.pl -Cperl5 src5/lib/Perlito5/Go/Emitter.pm      > lib5-new/Perlito5/Go/Emitter.pm
 perl perlito5.pl -Cperl5 src5/lib/Perlito5/Java/Emitter.pm    > lib5-new/Perlito5/Java/Emitter.pm
 perl perlito5.pl -Cperl5 src5/lib/Perlito5/Javascript/Emitter.pm > lib5-new/Perlito5/Javascript/Emitter.pm
-perl perlito5.pl -Cperl5 src5/lib/Perlito5/Lisp/Emitter.pm    > lib5-new/Perlito5/Lisp/Emitter.pm
-perl perlito5.pl -Cperl5 src5/lib/Perlito5/Parrot/Emitter.pm  > lib5-new/Perlito5/Parrot/Emitter.pm
 perl perlito5.pl -Cperl5 src5/lib/Perlito5/Python/Emitter.pm  > lib5-new/Perlito5/Python/Emitter.pm
 perl perlito5.pl -Cperl5 src5/lib/Perlito5/Ruby/Emitter.pm    > lib5-new/Perlito5/Ruby/Emitter.pm
 
@@ -53,16 +45,6 @@ cp src5/lib/Perlito5/Python/Runtime.py     lib5-new/Perlito5/Python/Runtime.py
 
 perl perlito5.pl -Cperl5 src5/lib/Perlito5/Javascript/Prelude.pm   > lib5-new/Perlito5/Javascript/Prelude.pm
 
-# older backends we want to keep around for now
-
-cp src5/lib/Perlito5/Go/Runtime.go         lib5-new/Perlito5/Go/Runtime.go
-cp src5/lib/Perlito5/Lisp/Runtime.lisp     lib5-new/Perlito5/Lisp/Runtime.lisp
-
-perl perlito5.pl -Cperl5 src5/lib/Perlito5/Clojure/Emitter.pm  > lib5-new/Perlito5/Clojure/Emitter.pm
-perl perlito5.pl -Cperl5 src5/lib/Perlito5/Go/Prelude.pm       > lib5-new/Perlito5/Go/Prelude.pm
-perl perlito5.pl -Cperl5 src5/lib/Perlito5/Lisp/Prelude.pm     > lib5-new/Perlito5/Lisp/Prelude.pm
-perl perlito5.pl -Cperl5 src5/lib/Perlito5/Parrot/Match.pm     > lib5-new/Perlito5/Parrot/Match.pm
-perl perlito5.pl -Cperl5 src5/lib/Perlito5/Rakudo/Emitter.pm   > lib5-new/Perlito5/Rakudo/Emitter.pm
 
 # clean up
 
