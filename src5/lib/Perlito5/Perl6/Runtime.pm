097a2e44cd2ae4564683a5a26914b5bb033fa55d
Flavio S. Glock <fglock@gmail.com>
2012-02-25 21:03:15 +0100

    Perlito5 - perl6: alternate bless() impl (not_gerd++)

--- src5/lib/Perlito5/Perl6/Runtime.pm
+++ src5/lib/Perlito5/Perl6/Runtime.pm
@@ -3,11 +3,20 @@ use v6;
 
 module Perlito5::Perl6::Runtime {
 
-# class X { has $.val }; my $v = {}; bless($v, "X"); $v.WHAT.say
 
-sub bless ($v is rw, $class) {
-    $v = ::($class).new(val=>$v)
+# bless() contributed by not_gerd in #perl6
+#
+# role Foo { method bar { say self<spam>; } }; 
+# my $x = { spam => 'eggs' };
+# bless5($x, 'Foo'); $x.bar;
+sub bless ($obj, $role) {
+    $obj does ::($role)
 }
+
+# class X { has $.val }; my $v = {}; bless($v, "X"); $v.WHAT.say
+# sub bless ($v is rw, $class) {
+#     $v = ::($class).new(val=>$v)
+# }
 
 sub ref ($obj) {
     my $s = $obj.WHAT.perl;
