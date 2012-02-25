3f7a9a81bb5c06ded55d672e687028381b582297
Flavio S. Glock <fglock@gmail.com>
2012-02-25 20:24:13 +0100

    Perlito5 - perl6: precompile Test.pm on setup

--- util-perl6/setup-perlito5-perl6.sh
+++ util-perl6/setup-perlito5-perl6.sh
@@ -7,3 +7,5 @@ mkdir lib6/Perlito5/Perl6
 
 cp src5/lib/Perlito5/Perl6/Runtime.pm lib6/Perlito5/Perl6/Runtime.pm
 
+perl -Ilib5 perlito5.pl -Cperl6 src5/lib/Perlito5/Test.pm  >  lib6/Perlito5/Test.pm
+
