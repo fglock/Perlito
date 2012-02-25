4b766889a29b290ff5e3358b7d1c44a7a4b37db6
Flavio S. Glock <fglock@gmail.com>
2012-02-25 19:55:48 +0100

    Perlito5 - perl6: add bless()

--- src5/lib/Perlito5/Perl6/Runtime.pm
+++ src5/lib/Perlito5/Perl6/Runtime.pm
@@ -3,6 +3,12 @@ use v6;
 
 module Perlito5::Perl6::Runtime {
 
+# class X { has $.val }; my $v = {}; bless($v, "X"); $v.WHAT.say
+
+sub bless ($v is rw, $class) {
+    $v = (eval $class).new(val=>$v)
+}
+
 sub ref ($obj) {
     my $s = $obj.WHAT.perl;
     return 'ARRAY' if $s eq 'Array';
@@ -24,7 +30,7 @@ Perlito5::Perl5::Runtime
 
 =head1 DESCRIPTION
 
-Provides runtime routines for the Perlito-in-Perl5 compiled code
+Provides runtime routines for the Perlito5-in-Perl6 compiled code
 
 =head1 AUTHORS
 
