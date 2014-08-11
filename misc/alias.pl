c3003cf9ff8a5339220f6f03e6ea3bf1c10454c8
Flavio S. Glock <fglock@gmail.com>
2014-08-11 11:44:21 +0200

    misc - add a parameter aliasing example

--- misc/alias.pl
+++ misc/alias.pl
@@ -12,3 +12,22 @@ my $tmp = abc($v);
 $v = $tmp->[1][0];  # "alias" $v to $_[0]
 $x = $tmp->[0];     # use the returned value
 
+# alternately,
+#   variables always have SCALAR/ARRAY/HASH properties
+#   @_ is special; it is always an array of aliases
+# pseudocode:
+
+var x = { SCALAR: null, ARRAY: [], HASH: {} }
+
+function (List__, p5want) {
+            List__.p5aset(0, 3);
+            return (List__.p5aget(0));
+        };
+
+tmp.SCALAR = abc(
+        [ x,             # pass by reference
+          { SCALAR: 3 }, # put value in a container
+        ], 
+        0
+);
+
