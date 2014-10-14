ef7a08d327661aa236570eecc4a1d5bbfb2e0551
Flavio S. Glock <fglock@gmail.com>
2014-10-14 09:34:22 +0200

    Perlito5 - typeglob vs. import behaviour investigation

--- misc/typeglob/import.pl
+++ misc/typeglob/import.pl
@@ -1,11 +1,35 @@
+#
+# importing variables like "our"/"use vars" - using typeglobs:
+#
+# The variables must be created in a *different* package
+#
+# The variables must be created at BEGIN time
+
+package X;
+# sub imp { my $x; *X::v = \$x; }
+
+package Y;
+use strict;
+
+# BEGIN { X::imp() }
+
+# BEGIN { my $x; *X::v = \$x; }
+
+# my $x; *X::v = \$x;
+sub imp { my $x; *X::v = \$x; }
+
 package X;
 use strict;
-no strict "refs";
 
+# no strict "refs";
 # use import;
-BEGIN { require "import.pm"; import::import( "import" ); }
+# BEGIN { require "import.pm"; import::import( "import" ); }
 # BEGIN { require "import.pm"; import::export( "import" ); }
 # BEGIN { my $x; *X::v = \$x; }
+
+BEGIN { Y::imp() }
+
+# Y::imp();
 
 $v
 
