164076c0076b66d7ed4e9d2cd4a6b704dc4b6d3f
Flavio S. Glock <fglock@gmail.com>
2014-11-09 20:47:38 +0100

    misc - dumper script tweak

--- misc/typeglob/dumper.pl
+++ misc/typeglob/dumper.pl
@@ -4,14 +4,24 @@ use Data::Dump::Streamer;
     no strict 'refs';
 
     my %out;
+    my %done;
+    my @todo = 'main::';
 
-    foreach my $entry ( keys %Exporter:: ) {
-    # foreach my $entry ( keys %main:: ) {
-        local *g = $Exporter::{$entry};
-        if ( defined *g{SCALAR} ) { $out{$entry}{SCALAR} = *g{SCALAR} }
-        if ( defined *g{ARRAY} )  { $out{$entry}{ARRAY}  = *g{ARRAY} }
-        if ( defined *g{HASH} )   { $out{$entry}{HASH}   = *g{HASH} }
-        if ( defined *g{CODE} )   { $out{$entry}{CODE}   = *g{CODE} }
+    while (@todo) {
+        my $namespace = shift(@todo);
+        next if $done{$namespace};
+        foreach my $entry ( keys %{$namespace} ) {
+            if ( length($entry) > 2 && substr( $entry, -2, 2 ) eq '::' ) {
+                push @todo, $entry;
+            }
+            local *g = $Exporter::{$entry};
+            if ( defined *g{SCALAR} ) { $out{$namespace}{$entry}{SCALAR} = *g{SCALAR} }
+            if ( defined *g{ARRAY} )  { $out{$namespace}{$entry}{ARRAY}  = *g{ARRAY} }
+            if ( defined *g{HASH} )   { $out{$namespace}{$entry}{HASH}   = *g{HASH} }
+            if ( defined *g{CODE} )   { $out{$namespace}{$entry}{CODE}   = *g{CODE} }
+        }
+        $done{$namespace} = 1;
+
     }
     print Dump( \%out ), "\n";
 }
