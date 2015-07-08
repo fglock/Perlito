3ef4243f7d3b12855835832a6d2f8aa5b36edf50
Flavio S. Glock <fglock@gmail.com>
2015-07-08 11:04:22 +0200

    Perlito5 - java - infrastructure for testing

---
+++ misc/Java/Test.pl
@@ -0,0 +1,47 @@
+#
+#   $ perl -I src5/lib misc/Java/Test.pl > Test.java
+#   $ javac Test.java
+#   $ java Test
+#
+
+use Perlito5::Java::Runtime;
+
+print Perlito5::Java::Runtime->emit_java();
+
+print <<'EOT';
+
+class Test {
+    public static void main(String[] args) {
+
+        PerlitoString s = new PerlitoString("456");
+        PerlitoInt i = new PerlitoInt(123);
+        PerlitoNum n = new PerlitoNum(123.456);
+        PerlitoBool t = new PerlitoBool(true);
+        PerlitoBool f = new PerlitoBool(false);
+        PerlitoObject x;
+
+        x = i.add(s);
+        x = x.add( new PerlitoInt(4) );
+        x.the_int_method();
+        s.the_int_method();
+        System.out.println(x.to_string());
+        x = s.add(i);
+        System.out.println(x.to_string());
+        x = s.add(n);
+        System.out.println(x.to_string());
+        System.out.println(t.to_string());
+        System.out.println(f.to_string());
+
+        PerlitoClosure c = new PerlitoClosure(s) {
+                public PerlitoObject apply() {
+                    System.out.println("called MyClosure with " + this.env.to_string());
+                    return new PerlitoInt(0);
+                }
+            };
+        c.apply();
+    }
+}
+
+EOT
+
+
