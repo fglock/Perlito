9bddad880b0e4f48558496cd49c26313680b1d6e
Flavio S. Glock <fglock@gmail.com>
2015-07-21 20:35:42 +0200

    Perlito5 - java - benchmark

---
+++ misc/Java/benchmark.pl
@@ -0,0 +1,18 @@
+my $count;
+
+my $i = 0;
+while ( $i < 400 ) {
+    my $j = 0;
+    while ( $j < 400 ) {
+        my $k = 0;
+        while ( $k < 400 ) {
+            $k = $k + 1;
+            $count = $count + 1;
+        }
+        $j = $j + 1;
+    }
+    $i = $i + 1;
+}
+
+print "done $count\n";
+
