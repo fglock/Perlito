efeda2c7b0b2cb4d2998bdb45eea4c0c8ba5048b
Flavio S. Glock <fglock@gmail.com>
2015-07-22 14:33:38 +0200

    Perlito5 - java - fix indexOf test

--- src5/lib/Perlito5/Java/Runtime.pm
+++ src5/lib/Perlito5/Java/Runtime.pm
@@ -1790,14 +1790,14 @@ EOT
             if ($returns eq 'pNum') {
 "    public pObject ${perl}(pObject b) {
         // 'num' - int, 'num' - num
-        if (this.s.indexOf('.') > 0) {
+        if (this.s.indexOf('.') > -1) {
             return new pNum( this.to_num() ${native} b.to_num() );
         }
         return new pInt( this.to_int() ${native} b.to_int() );
     }
     public pObject ${perl}2(pObject b) {
         // int - 'num'
-        if (this.s.indexOf('.') > 0) {
+        if (this.s.indexOf('.') > -1) {
             return new pNum( b.to_num() ${native} this.to_num() );
         }
         return new pInt( b.to_int() ${native} this.to_int() );
@@ -1807,14 +1807,14 @@ EOT
             else {
 "    public pObject ${perl}(pObject b) {
         // 'num' - int, 'num' - num
-        if (this.s.indexOf('.') > 0) {
+        if (this.s.indexOf('.') > -1) {
             return new ${num_returns}( this.to_num() ${native} b.to_num() );
         }
         return b.${perl}2(new pInt(this.to_int()));
     }
     public pObject ${perl}2(pObject b) {
         // int - 'num'
-        if (this.s.indexOf('.') > 0) {
+        if (this.s.indexOf('.') > -1) {
             return new ${num_returns}( b.to_num() ${native} this.to_num() );
         }
         return new ${returns}( b.to_int() ${native} this.to_int() );
