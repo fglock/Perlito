7e76d83b6302d1fcb55a5a73ec46d31782bed91e
Flavio S. Glock <fglock@gmail.com>
2015-07-30 16:11:02 +0200

    Perlito5 - java - HBase example

---
+++ misc/Java/HBase.pl
@@ -0,0 +1,26 @@
+package IOException { import => "java.io.IOException" };
+package Configuration { import => "org.apache.hadoop.conf.Configuration" };
+package HBaseConfiguration { import => "org.apache.hadoop.hbase.HBaseConfiguration" };
+package Get { import => "org.apache.hadoop.hbase.client.Get" };
+package HTable { import => "org.apache.hadoop.hbase.client.HTable" };
+package Result { import => "org.apache.hadoop.hbase.client.Result" };
+package Bytes { import => "org.apache.hadoop.hbase.util.Bytes" };
+
+# Instantiating Configuration class
+my Configuration $config = HBaseConfiguration->create();
+
+# Instantiating HTable class
+my HTable $table = new HTable( $config, "b_room" );
+
+# Instantiating Get class
+my Get $g = new Get( Bytes->toBytes("100002303") );
+
+# Reading the data
+my Result $result = $table->get($g);
+
+# Reading values from Result class object
+my $max_price = Bytes->toString( $result->getValue( Bytes->toBytes("d"), Bytes->toBytes("max_price") ) );
+
+# Printing the values
+say "max price: " . $max_price;
+
