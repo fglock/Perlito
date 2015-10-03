1ac279ff46e7210dac7053cc8745e32e3fe9ca02
Flavio S. Glock <fglock@gmail.com>
2015-10-03 16:55:27 +0200

    Perlito5 - Java - add Rhino JS integration example

--- misc/Java/TestJS.pl
+++ misc/Java/TestJS.pl
@@ -1,3 +1,4 @@
+# here we import the Java packages into Perl packages:
 package ScriptEngineManager {
     import => "javax.script.ScriptEngineManager"
 };
@@ -5,11 +6,12 @@ package ScriptEngine {
     import => "javax.script.ScriptEngine"
 };
 
+# these are Perl variables, but they are also Java objects
+# so we have to declare which package they belong to:
 my ScriptEngineManager $manager = new ScriptEngineManager();
 my ScriptEngine $engine = $manager->getEngineByName("JavaScript");
 
+# now we can call the Rhino javascript engine:
 $engine->eval(
     'print( "JS thinks that the result is " + ( 1+1 ) + "\n" );'
 );
-
-
