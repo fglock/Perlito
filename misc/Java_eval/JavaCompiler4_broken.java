b1fe786ec051c781b1d9986a796cac6a1e9f0fe2
Flavio S. Glock <fglock@gmail.com>
2015-10-12 14:17:49 +0200

    Perlito5 - java - misc/Java_eval/ - test

--- misc/Java_eval/JavaCompiler4_broken.java
+++ misc/Java_eval/JavaCompiler4_broken.java
@@ -3,13 +3,25 @@
 //              <url>http://www.apache.org/licenses/LICENSE-2.0.txt</url>
 // https://github.com/turpid-monkey/InMemoryJavaCompiler
 //
-import org.mdkt.compiler.InMemoryJavaCompiler;
-import org.mdkt.compiler.ExtendedStandardJavaFileManager;
 
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
+import java.net.URI;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
-
+import javax.tools.FileObject;
+import javax.tools.ForwardingJavaFileManager;
+import javax.tools.JavaCompiler;
+import javax.tools.JavaFileManager;
+import javax.tools.JavaFileObject;
+import javax.tools.SimpleJavaFileObject;
+import javax.tools.ToolProvider;
 
 
 public class JavaCompiler4_broken
@@ -93,3 +105,198 @@ public class JavaCompiler4_broken
         method.invoke(null, aaa);
     }
 }
+
+
+/**
+ * Created by trung on 5/3/15.
+ * Edited by turpid-monkey on 9/25/15, added support for multiple, dependent compile units.
+ */
+class InMemoryJavaCompiler {
+	JavaCompiler javac;
+	DynamicClassLoader classLoader;
+
+	Map<String, SourceCode> clazzCode = new HashMap<String, SourceCode>();
+
+	public InMemoryJavaCompiler(ClassLoader parent) {
+		this(ToolProvider.getSystemJavaCompiler(), parent);
+	}
+
+	public InMemoryJavaCompiler(JavaCompiler javac, ClassLoader parent) {
+		this.javac = javac;
+		this.classLoader = new DynamicClassLoader(parent);
+	}
+
+	public InMemoryJavaCompiler() {
+		this(ToolProvider.getSystemJavaCompiler(), ClassLoader
+				.getSystemClassLoader());
+	}
+
+	public void addSource(String className, String sourceCodeInText)
+			throws Exception {
+		clazzCode.put(className, new SourceCode(className, sourceCodeInText));
+	}
+
+	public Map<String, Class<?>> compileAll() throws Exception {
+		Collection<SourceCode> compilationUnits = clazzCode.values();
+		CompiledCode[] code;
+		
+		code = new CompiledCode[compilationUnits.size()];
+		Iterator<SourceCode> iter = compilationUnits.iterator();
+		for (int i=0; i<code.length; i++)
+		{
+			code[i] = new CompiledCode(iter.next().getClassName());
+		}
+		
+		ExtendedStandardJavaFileManager fileManager = new ExtendedStandardJavaFileManager(
+				javac.getStandardFileManager(null, null, null), classLoader, code);
+		JavaCompiler.CompilationTask task = javac.getTask(null, fileManager,
+				null, null, null, compilationUnits);
+		boolean result = task.call();
+		if (!result)
+			throw new RuntimeException("Unknown error during compilation.");
+		Map<String, Class<?>> classes = new HashMap<String, Class<?>>();
+		for (String className : clazzCode.keySet()) {
+			classes.put(className, classLoader.loadClass(className));
+		}
+		return classes;
+	}
+
+	public static Class<?> compile(String className, String sourceCodeInText)
+			throws Exception {
+		InMemoryJavaCompiler comp = new InMemoryJavaCompiler();
+		comp.addSource(className, sourceCodeInText);
+		Map<String, Class<?>> clzzes = comp.compileAll();
+		Class<?> result = clzzes.get(className);
+		return result;
+	}
+	
+	public DynamicClassLoader getClassLoader() {
+		return classLoader;
+	}
+}
+
+
+/**
+ * Created by trung on 5/3/15.
+ * Edited by turpid-monkey on 9/25/15, completed support for multiple compile units.
+ */
+class ExtendedStandardJavaFileManager extends
+		ForwardingJavaFileManager<JavaFileManager> {
+
+	private CompiledCode[] compiledCode;
+	private DynamicClassLoader cl;
+
+	/**
+	 * Creates a new instance of ForwardingJavaFileManager.
+	 *
+	 * @param fileManager
+	 *            delegate to this file manager
+	 * @param cl
+	 */
+	protected ExtendedStandardJavaFileManager(JavaFileManager fileManager,
+			DynamicClassLoader cl, CompiledCode... compiledCode) {
+		super(fileManager);
+		this.compiledCode = compiledCode;
+		this.cl = cl;
+		for (CompiledCode code : compiledCode) {
+			this.cl.addCode(code);
+		}
+	}
+
+	@Override
+    public JavaFileObject getJavaFileForOutput(JavaFileManager.Location location, String className, JavaFileObject.Kind kind, FileObject sibling) throws IOException {
+    	for (CompiledCode code : compiledCode)
+    	{
+    		if (code.getClassName().equals(className)) return code;
+    	}
+    	throw new FileNotFoundException("Missing source code for class " + className );
+    }
+
+	@Override
+	public ClassLoader getClassLoader(JavaFileManager.Location location) {
+		return cl;
+	}
+}
+
+
+/**
+ * Created by trung on 5/3/15.
+ */
+class CompiledCode extends SimpleJavaFileObject {
+    private ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    private String className;
+
+    public CompiledCode(String className) throws Exception {
+        super(new URI(className), Kind.CLASS);
+        this.className = className;
+    }
+    
+    public String getClassName() {
+		return className;
+	}
+
+    @Override
+    public OutputStream openOutputStream() throws IOException {
+        return baos;
+    }
+
+    public byte[] getByteCode() {
+        return baos.toByteArray();
+    }
+}
+
+
+
+
+
+/**
+ * Created by trung on 5/3/15.
+ */
+class DynamicClassLoader extends ClassLoader {
+
+    private Map<String, CompiledCode> customCompiledCode = new HashMap<String, CompiledCode>();
+
+    public DynamicClassLoader(ClassLoader parent) {
+        super(parent);
+    }
+
+    public void addCode(CompiledCode cc) {
+        customCompiledCode.put(cc.getName(), cc);
+    }
+
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        CompiledCode cc = customCompiledCode.get(name);
+        if (cc == null) {
+            return super.findClass(name);
+        }
+        byte[] byteCode = cc.getByteCode();
+        return defineClass(name, byteCode, 0, byteCode.length);
+    }
+}
+
+
+/**
+ * Created by trung on 5/3/15.
+ */
+class SourceCode extends SimpleJavaFileObject {
+	private String contents = null;
+	private String className;
+
+	public SourceCode(String className, String contents) throws Exception {
+		super(URI.create("string:///" + className.replace('.', '/')
+				+ Kind.SOURCE.extension), Kind.SOURCE);
+		this.contents = contents;
+		this.className = className;
+	}
+
+	public String getClassName() {
+		return className;
+	}
+
+	public CharSequence getCharContent(boolean ignoreEncodingErrors)
+			throws IOException {
+		return contents;
+	}
+}
+
