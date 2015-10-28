dc5f039e4bd80818e754ab7bba27f370db6bc551
Flavio S. Glock <fglock@gmail.com>
2015-10-28 14:09:12 +0100

    Perlito5 - BEGIN - compile-time wip

--- src5/lib/Perlito5/CompileTime/Closure.pm
+++ src5/lib/Perlito5/CompileTime/Closure.pm
@@ -17,3 +17,14 @@ sub dump {
     # do { my $x = 123; sub { $x + $_[0] } }
 }
 
+# TODO - problem with identifying shared variables in js,
+# because it doesn't use "real" scalar references
+#
+# $ node perlito5.js -I src5/lib -e ' my $x = 123; my $v = \$x; my @x = ( $v, \$x ); print $_,"\n" for @x '
+# SCALAR(0x5b143b76)
+# SCALAR(0x5b143b77)    -- different ref value
+#
+# $ perl -e ' use Data::Dumper; my $x = 123; my $v = \$x; my @x = ( $v, \$x ); print $_,"\n" for @x '
+# SCALAR(0x7fc01902af60)
+# SCALAR(0x7fc01902af60)
+#
