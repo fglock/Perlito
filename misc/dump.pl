bbb1bb9e57ef5888142cba78bd651a4c743598d7
Flavio S. Glock <fglock@gmail.com>
2015-10-29 14:23:13 +0100

    Perlito5 - BEGIN - side-effect model wip

--- misc/dump.pl
+++ misc/dump.pl
@@ -2,7 +2,7 @@ BEGIN {
     for my $x ( 4 .. 6 ) {
 
         *{"mm$x"} = sub {
-            if ( $_[0] eq "dump" ) { return "do { my \$x = $x; sub { \$_[0] + \$x;  } }" }
+            if ( @_ && ref($_[0]) eq "Perlito5::dump" ) { return "do { my \$x = $x; sub { \$_[0] + \$x;  } }" }
             $_[0] + $x;
           };
 
@@ -11,15 +11,17 @@ BEGIN {
 
 BEGIN {
     
+    my $flag = bless {}, "Perlito5::dump";
+
     print "compile-time\n";
 
     my $y = \&mm4;
-    print $y->("dump"), "\n", $y->(456), " ", mm4(0), "\n";
+    print $y->($flag), "\n", $y->(456), " ", mm4(0), "\n";
 
     $y = \&mm5;
-    print $y->("dump"), "\n", $y->(456), "\n";
+    print $y->($flag), "\n", $y->(456), "\n";
 
     $y = \&mm6;
-    print $y->("dump"), "\n", $y->(456), "\n";
+    print $y->($flag), "\n", $y->(456), "\n";
 }
 
