e14e8058e91c466bff05b3e07568271d596a41ad
Flavio S. Glock <fglock@gmail.com>
2015-12-04 13:31:56 +0100

    Perlito5 - java - new example code

---
+++ misc/Java/Calendar.pl
@@ -0,0 +1,22 @@
+# this Perl example was translated from this Java source code:
+# http://www.java2s.com/Tutorial/Java/0120__Development/TimeZonegetTimeZoneAmericaNewYork.htm
+#
+# usage:
+#
+# $ perl perlito5.pl -Isrc5/lib -I. -It -Cjava misc/Java/Calendar.pl > Main.java ; javac Main.java ; java Main
+# Time in New York: 7:30
+#
+
+use feature 'say';
+use strict;
+package Calendar { import => "java.util.Calendar" }
+package TimeZone { import => "java.util.TimeZone" }
+
+my Calendar $calNewYork = Calendar->getInstance();
+
+$calNewYork->setTimeZone(TimeZone->getTimeZone("America/New_York"));
+
+my $hour = $calNewYork->get(Calendar->HOUR_OF_DAY);
+my $min  = $calNewYork->get(Calendar->MINUTE);
+say "Time in New York: $hour:$min";
+
