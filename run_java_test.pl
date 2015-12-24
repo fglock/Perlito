2e218fb73caf22b08cc9e1eecc4c2bd52c8faedc
Flavio S. Glock <fglock@gmail.com>
2015-12-24 14:38:22 +0100

    Perlito5 - docs

--- run_java_test.pl
+++ run_java_test.pl
@@ -1,3 +1,10 @@
+# run_java_test.pl
+#
+# Use this script together with "prove" to run Perlito5-Java tests:
+#
+# prove -r -e 'perl run_java_test.pl' t5
+#
+
 my $t = shift;
 print ` touch Main.class `;
 print ` rm Main.class `;
