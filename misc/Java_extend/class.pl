b5d7f3259f85e1c44252954d8ed07bb285001eb1
Flavio S. Glock <fglock@gmail.com>
2015-12-28 17:10:25 +0100

    Perlito5 - java - "extends" example wip

--- misc/Java_extend/class.pl
+++ misc/Java_extend/class.pl
@@ -6,7 +6,12 @@ package MyClass {
     extends => 'JavaObject';
     sub MODIFY_CODE_ATTRIBUTES   { }
 
-    sub meth :public :Int {
+    sub instance_meth :public :Int {
+        my MyClass $self = shift;
+        my Int $param1 = shift;
+        return $param1 + 1;
+    }
+    sub class_meth :public :static :Int {
         my Int $param1 = shift;
         return $param1 + 1;
     }
@@ -17,11 +22,33 @@ package MyClass {
 
 __END__
 
+    # generated Java code:
+
     class MyClass extends java.lang.Object {
-        public Int meth(Int param1) throws Exception {
-            PlObject[] res = Main.apply("MyClass::meth", param1);
+        public Int instance_meth(Int param1) throws Exception {
+            PlObject[] res = Main.apply("MyClass::instance_meth", this, param1);
+            return res[0].to_Int();
+        }
+        public Int class_meth(Int param1) throws Exception {
+            PlObject[] res = Main.apply("MyClass::class_meth", param1);
             return res[0].to_Int();
         }
     }
+
+
+
+    # possible Perl "macros" for shortening
+
+    use Java::class MyClass => {
+        extends => 'JavaObject',
+    
+        public_Int(instance_meth => {
+            $this # avaiable
+        })
+    
+        public_static_Int(class_meth => {
+            # $this not available
+        })
+    };
 
 
