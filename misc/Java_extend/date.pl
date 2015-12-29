d2a457268879dea49525be5d3890c29c95d23aee
Flavio S. Glock <fglock@gmail.com>
2015-12-29 16:40:26 +0100

    Perlito5 - java - "extends" wip

--- misc/Java_extend/date.pl
+++ misc/Java_extend/date.pl
@@ -4,7 +4,7 @@ package J::Date   { import => "java.util.Date" };
 package My::Date {
     extends => 'J::Date',
     methods => [
-        extra_data => {
+        toString => {
             decl => [ "public", "String" ],
             args => [],
             code => "main::date_string",
@@ -21,7 +21,7 @@ sub date_string {
 my J::Date $j_date = J::Date->new();
 my $s1 = $j_date->toString();
 my My::Date $date = My::Date->new();
-my $s2 = $date->extra_data();
+my $s2 = $date->toString();
 
 print $s1, " ", $s2, "\n";   # date / hello
 
