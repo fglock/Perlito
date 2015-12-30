9a5fc7d8395de6d5405216b91648a31ad9cc36b9
Bosko Devetak <bosko.devetak@gmail.com>
2015-12-30 12:44:10 +0100

    Some examples and todo's for types

--- misc/Java/Types2.pl
+++ misc/Java/Types2.pl
@@ -2,7 +2,7 @@ use strict;
 
 package Byte { }
 
-package ByteList {
+package ByteArray {
     import    => "java.util.ArrayList",
     java_type => "ArrayList<Byte>",
 } 
@@ -12,18 +12,39 @@ package ByteIterator {
     java_type => "Iterator<Byte>",
 }
 
-# create java ArrayList<Byte>
-my ByteList $bar = ByteList->new();
+# list of raw java stuff
+my ByteArray $bar = ByteArray->new();
 
-# copy perl array to java ArrayList<Byte>
-my @perlArray = (100, 65, 88);
-for (@perlArray) {
-     my Byte $x = $_->to_byte();
-     $bar->add($x);  
-}
+my Byte $x = 100; # java native
+my Byte $y = 65;
+my Byte $z = 88;
 
+##################################################
+# TODO: copy from perl array to java arrayList
+#
+#   my @perlArray = (100, 200, 255);
+#   map {
+#       my Byte $x = $_->to_byte(); $bar->add($x);    
+#   } @perlArray;
+#
+$bar->add($x); # <- this adds raw byte to list
+$bar->add($y);
+$bar->add($z);
+
+##################################################
+# TODO: provide iterator with @$bar
+#   my ByteIterator $barIterator = @$bar;
 my ByteIterator $barIterator = $bar->iterator();
 
+##################################################
+# TODO: automatic call to ->hasNext in boolean context
+#
+#   while (@$bar) { ... }
+# 
+#   This would obtain iterator with @$bar and then due
+#   to boolean context would call hasNext().
+#   That way we will be able to write idiomatic Perl
+#   loops on native java Lists
 while($barIterator->hasNext()) {
     
     # get byte in perl varl
