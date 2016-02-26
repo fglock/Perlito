c56781e9876eac7aed3d0fa179bd4fcbafe4319a
Yati Sagade <yati.sagade@gmail.com>
2016-02-26 15:38:36 +0100

    Use Perlito5::TreeGrammar to traverse and mutate the AST. Also, documentation

--- src5/lib/Perlito5/TreeGrammar.pm
+++ src5/lib/Perlito5/TreeGrammar.pm
@@ -11,13 +11,35 @@ my %dispatch = (
         Or     => sub { Or(@_) },
         Not    => sub { Not(@_) },
         Action => sub { Action(@_) },
-        Progn  => sub { Progn(@_) },     # same as in Lisp
-        Star   => sub { Star(@_) },      # same as in regex
+        Progn  => sub { Progn(@_) },     # same as in Lisp (a list of statements to execute)
+        Star   => sub { Star(@_) },      # same as in regex (matches anything)
     );
 
+# render($rule, $node)
+#
+# This is the entry point for walking an AST. The first argument
+# is the macro rule to process, which can be thought of as a
+# regular expression for trees, which means the rule itself is
+# composed of smaller sub-rules.
+#
+# $node is the AST itself, which render() tries to match against
+# $rule.
+#
+# Each rule is just a list, with the first element being its name,
+# which in turn is one of the rulenames defined in %dispatch above.
+# All elements following the rulename in a rule are interpreted as
+# arguments to the rule. For instance,
+# 
+#     [And => [Ref => 'Perlito5::AST::Block']
+#             [Lookup => 'stmts']
+#             [Index => 0]
+#             [Action => sub { say Data::Dumper::Dumper $_[0]; }]]
+#
+# Will dump the first statement of a Block, if it is not empty.
+#
 sub render {
     my ( $rule, $node ) = @_;
-    return $dispatch{ $rule->[0] }->( $rule, $node );
+    return $dispatch{ $rule->[0] }->( $rule, $node ); # note that each rule handler gets the whole rule (including its name) as first argument.
 }
 
 sub scan {
@@ -93,6 +115,10 @@ sub Lookup {
          if !ref($node)
       || ref($node) eq 'ARRAY'
       || !exists( $node->{ $rule->[1] } );
+    # Typically, $rule->[2] contains the action to execute on a
+    # successful lookup (although it could be an arbitrary rule).
+    # If nothing is present, however, we just return 1 indicating
+    # a successful match, else we recurse into the following rule.
     return 1 if !$rule->[2];
     return render( $rule->[2], $node->{ $rule->[1] } );
 }
@@ -103,6 +129,7 @@ sub Index {
          if !ref($node)
       || ref($node) ne 'ARRAY'
       || !exists( $node->[ $rule->[1] ] );
+    # See the corresponding section in Lookup()
     return 1 if !$rule->[2];
     return render( $rule->[2], $node->[ $rule->[1] ] );
 }
