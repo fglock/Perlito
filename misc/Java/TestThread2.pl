9a78c98035977175087596d31d1fff0baaa9fd18
Flavio S. Glock <fglock@gmail.com>
2016-04-14 14:48:21 +0200

    Perlito5 - java - bigger "Thread" example

--- misc/Java/TestThread2.pl
+++ misc/Java/TestThread2.pl
@@ -1,42 +1,33 @@
-package Java::Thread { import => "java.lang.Thread" };
-
-package Java::AtomicInteger { import => "java.util.concurrent.atomic.AtomicInteger" };
-
-my $shared = 0;
-my $atomic = Java::AtomicInteger->new(0);
+package Java::Thread {
+    import => "java.lang.Thread"
+};
 
 eval {
     my Java::Thread $thread1 = Java::Thread->new(
         sub {
-            for my $i ( 0 .. 5 ) {
-                $shared++;                                             # unsafe!
-                $atomic->to_JavaAtomicInteger()->incrementAndGet();    # safe
-                print "thread 1; shared $shared\n";
-                sleep(1);
+            for my $i (0..5) {
+                print "thread 1\n";
+                sleep (1);
             }
         }
     );
     my Java::Thread $thread2 = Java::Thread->new(
         sub {
-            for my $i ( 0 .. 5 ) {
-                $shared++;                                             # unsafe!
-                $atomic->to_JavaAtomicInteger()->incrementAndGet();    # safe
-                print "thread 2; shared $shared\n";
-                sleep(1);
+            for my $i (0..5) {
+                print "thread 2\n";
+                sleep (1);
             }
         }
     );
-
+    
     $thread1->start();
     $thread2->start();
-
+    
     $thread1->join();
     $thread2->join();
     1;
 }
-  or die $@;
+or die $@;
 
-my $value =
-  $atomic->to_JavaAtomicInteger()->get();   # transform from 'Java::AtomicInteger' into plain scalar
-print "done. shared $shared (unsafe) - $value (safe)\n";
+print "done\n";
 
