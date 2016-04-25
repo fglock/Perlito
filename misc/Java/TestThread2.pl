8c36623ba07ffaa394b6f9bd5b5c36e1b9eed25d
Flavio S. Glock <fglock@gmail.com>
2016-04-25 11:17:31 +0200

    Perlito5 - java - AtomicInteger example

--- misc/Java/TestThread2.pl
+++ misc/Java/TestThread2.pl
@@ -1,37 +1,42 @@
-package Java::Thread {
-    import => "java.lang.Thread"
-};
+package Java::Thread { import => "java.lang.Thread" };
+
+package Java::AtomicInteger { import => "java.util.concurrent.atomic.AtomicInteger" };
 
 my $shared = 0;
+my $atomic = Java::AtomicInteger->new(0);
 
 eval {
     my Java::Thread $thread1 = Java::Thread->new(
         sub {
-            for my $i (0..5) {
-                $shared++;  # unsafe!
+            for my $i ( 0 .. 5 ) {
+                $shared++;                                             # unsafe!
+                $atomic->to_JavaAtomicInteger()->incrementAndGet();    # safe
                 print "thread 1; shared $shared\n";
-                sleep (1);
+                sleep(1);
             }
         }
     );
     my Java::Thread $thread2 = Java::Thread->new(
         sub {
-            for my $i (0..5) {
-                $shared++;  # unsafe!
+            for my $i ( 0 .. 5 ) {
+                $shared++;                                             # unsafe!
+                $atomic->to_JavaAtomicInteger()->incrementAndGet();    # safe
                 print "thread 2; shared $shared\n";
-                sleep (1);
+                sleep(1);
             }
         }
     );
-    
+
     $thread1->start();
     $thread2->start();
-    
+
     $thread1->join();
     $thread2->join();
     1;
 }
-or die $@;
+  or die $@;
 
-print "done. shared $shared\n";
+my $value =
+  $atomic->to_JavaAtomicInteger()->get();   # transform from 'Java::AtomicInteger' into plain scalar
+print "done. shared $shared (unsafe) - $value (safe)\n";
 
