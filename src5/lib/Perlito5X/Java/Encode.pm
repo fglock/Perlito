0245dcc9e17bbc63da0a9b5f0632e62e348c493c
Flavio S. Glock <fglock@gmail.com>
2016-07-07 19:51:13 +0200

    Perlito5 - java - Encode.pm wip

--- src5/lib/Perlito5X/Java/Encode.pm
+++ src5/lib/Perlito5X/Java/Encode.pm
@@ -2,33 +2,39 @@ package Encode;
 use strict;
 
 sub encode_utf8 {
-    if (@_) {
-    Java::inline '
-        String s = List__.shift().toString();
-        byte[] bytes = s.getBytes("UTF-8");
-        StringBuilder sb = new StringBuilder();
-        for (byte b : bytes) {
-            sb.append((char)b);
+    eval {
+        if (@_) {
+        Java::inline '
+            String s = List__.shift().toString();
+            byte[] bytes = s.getBytes("UTF-8");
+            StringBuilder sb = new StringBuilder();
+            for (byte b : bytes) {
+                sb.append((char)b);
+            }
+            return new PlString(sb.toString())
+        ';
         }
-        return new PlString(sb.toString())
-    ';
+        return undef;
     }
-    return undef;
+    or die $@;
 }
 
 sub decode_utf8 {
-    if (@_) {
-    Java::inline '
-        String s = List__.shift().toString();
-        char[] chars = s.toCharArray();
-        byte[] bytes = new byte[chars.length];
-        for (int i = 0; i < bytes.length(); i++){
-            bytes[i] = (int)(chars[i]);
+    eval {
+        if (@_) {
+        Java::inline '
+            String s = List__.shift().toString();
+            char[] chars = s.toCharArray();
+            byte[] bytes = new byte[chars.length];
+            for (int i = 0; i < bytes.length; i++){
+                bytes[i] = (byte)(chars[i]);
+            }
+            return new PlString(new String(bytes, "UTF-8"))
+        ';
         }
-        return new PlString(new String(bytes, "UTF-8"));
-    ';
+        return undef;
     }
-    return undef;
+    or die $@;
 }
 
 1;
