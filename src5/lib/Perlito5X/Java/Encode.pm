e52026d32b043d426f33f92016755754af95da9e
Flavio S. Glock <fglock@gmail.com>
2016-07-10 10:56:10 +0200

    Perlito5 - java - Encode.pm wip

--- src5/lib/Perlito5X/Java/Encode.pm
+++ src5/lib/Perlito5X/Java/Encode.pm
@@ -5,24 +5,18 @@ use Exporter qw(import);
 our @EXPORT    = qw(encode_utf8 decode_utf8);
 our @EXPORT_OK = qw(encode_utf8 decode_utf8 encode decode);
 
-sub encode ($$;$) {
-    my ($code) = @_;
-    encode_utf8($_[1]) if $code eq 'utf8';
-    die "not implemented: $code";
-}
-
-sub decode ($$;$) {
-    my ($code) = @_;
-    decode_utf8($_[1]) if $code eq 'utf8';
-    die "not implemented: $code";
-}
-
 sub encode_utf8 ($) {
+    return encode('UTF-8', $_[0]);
+}
+
+sub encode ($$;$) {
+    return encode('UTF-8', $_[1], $_[2]) if $code eq 'utf8';
     eval {
         if (@_) {
         Java::inline '
+            String charset = List__.shift().toString();
             String s = List__.shift().toString();
-            byte[] bytes = s.getBytes("UTF-8");
+            byte[] bytes = s.getBytes(charset);
             StringBuilder sb = new StringBuilder();
             for (byte b : bytes) {
                 int i = b < 0 ? 256 + b : b;
@@ -37,16 +31,22 @@ sub encode_utf8 ($) {
 }
 
 sub decode_utf8 ($;$) {
+    return decode('UTF-8', $_[0], $_[1]);
+}
+
+sub decode ($$;$) {
+    return decode('UTF-8', $_[1], $_[2]) if $code eq 'utf8';
     eval {
         if (@_) {
         Java::inline '
+            String charset = List__.shift().toString();
             String s = List__.shift().toString();
             char[] chars = s.toCharArray();
             byte[] bytes = new byte[chars.length];
             for (int i = 0; i < bytes.length; i++){
                 bytes[i] = (byte)(chars[i]);
             }
-            return new PlString(new String(bytes, "UTF-8"))
+            return new PlString(new String(bytes, charset))
         ';
         }
         return undef;
