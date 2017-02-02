eaa2ca6188d81dfefbf056364675a33838d4202c
Flavio S. Glock <fglock@gmail.com>
2017-02-02 11:29:08 +0100

    Perlito5 - java - update calling-JavaScript example

--- misc/Java/TestJS.pl
+++ misc/Java/TestJS.pl
@@ -6,12 +6,17 @@ package ScriptEngine {
     import => "javax.script.ScriptEngine"
 };
 
-# these are Perl variables, but they are also Java objects
-# so we have to declare which package they belong to:
-my ScriptEngineManager $manager = ScriptEngineManager->new();
-my ScriptEngine $engine = $manager->getEngineByName("JavaScript");
+eval {
+    # these are Perl variables, but they are also Java objects
+    # so we have to declare which package they belong to:
+    my ScriptEngineManager $manager = ScriptEngineManager->new();
+    my ScriptEngine $engine = $manager->getEngineByName("JavaScript");
+    
+    # now we can call the Rhino javascript engine:
+    $engine->eval(
+        'print( "JS thinks that the result is " + ( 1+1 ) + "\n" );'
+    );
+    1;
+}
+  or print "JS returned error: $@\n";
 
-# now we can call the Rhino javascript engine:
-$engine->eval(
-    'print( "JS thinks that the result is " + ( 1+1 ) + "\n" );'
-);
