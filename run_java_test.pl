6fa71a2b461864b224262cecba0f2fe843a5e190
Flavio S. Glock <fglock@gmail.com>
2017-04-24 20:35:53 +0200

    Perlito5 - java - work around 'slow compilation' Java 8 bug

--- run_java_test.pl
+++ run_java_test.pl
@@ -9,6 +9,18 @@ my $t = shift;
 print ` touch Main.class `;
 print ` rm Main.class `;
 print ` perl perlito5.pl -Isrc5/lib -I. -It -Cjava $t  > Main.java `;
-print ` javac Main.java `;
+print ` javac -source 7 Main.java `;
 print ` java Main `;
+
+
+__END__
+
+Note: large source files seems to trigger this problem:
+
+http://stackoverflow.com/questions/30707387/troubleshoot-slow-compilation
+http://stackoverflow.com/questions/34223249/slow-compilation-with-jooq-3-6-plain-sql-and-the-javac-compiler
+
+- "The workaround is to compile at Java 7-compatibility level: javac -source 7, or just to use simpler constructions.
+- "the workaround is to introduce local variables when there are nested generic method calls that use generic type inference
+
 
