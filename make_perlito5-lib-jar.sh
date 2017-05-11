6b2254a3450e35fe8ddfa9eaaa3d5b50b052d2cc
Flavio S. Glock <fglock@gmail.com>
2017-05-11 13:31:27 +0200

    Perlito5 - java - "make build-5java"

--- make_perlito5-lib-jar.sh
+++ make_perlito5-lib-jar.sh
@@ -1,8 +1,14 @@
 
+# cleanup
+
 make clean
 rm perlito5-lib.jar
 rm -rf org
 
+
+# Step 1: compile and package the Perl lib
+
+# Note: no --java_eval
 perl perlito5.pl --bootstrapping --bootstrap_java_eval -Isrc5/lib -Cjava src5/util/perlito5-javalib.pl > perlito5-javalib.java
 
 mkdir org
@@ -11,14 +17,23 @@ mkdir org/perlito/Perlito5
 
 perl make_perlito5-lib-jar_.pl perlito5-javalib.java
 
-# cp src5/java/PlJavaCompiler.java org/perlito/Perlito5/
-
 time javac -J-Xms2000m -J-Xmx2000m -J-Xss2000m -source 7 org/perlito/Perlito5/LibPerl.java 
 jar -cfe perlito5-lib.jar org.perlito.Perlito5.LibPerl org/perlito/Perlito5/*.class
 
-# time javac -J-Xms2000m -J-Xmx2000m -J-Xss2000m -source 7 -cp perlito5-lib.jar org/perlito/Perlito5/PlJavaCompiler.java
-# rm perlito5-lib.jar
-# jar -cfe perlito5-lib.jar org.perlito.Perlito5.LibPerl org/perlito/Perlito5/*.class
-# rm -rf org
+
+
+# Step 2: compile and package the CLI
+
+# Note: use --java_eval
+perl perlito5.pl --bootstrapping --java_eval -Isrc5/lib -Cjava src5/util/jperl.pl > jperl.java
+
+perl make_perlito5-lib-jar_.pl jperl.java 
+
+# Note: compile with perlito5-lib.jar so eval-string works
+# Note: jperl.pl contains class "Main"
+javac -cp .:perlito5-lib.jar -source 7 org/perlito/Perlito5/Main.java
+
+# repackage everything
+jar -cfe perlito5.jar org.perlito.Perlito5.Main org/perlito/Perlito5/*.class
 
 
