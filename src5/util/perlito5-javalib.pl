18e2de9213d39350d2bcfc22c057c8a94941569a
Flavio S. Glock <fglock@gmail.com>
2017-05-16 20:15:48 +0200

    Perlito5 - java - add debug

--- src5/util/perlito5-javalib.pl
+++ src5/util/perlito5-javalib.pl
@@ -7,6 +7,12 @@ use Perlito5::Java::Emitter;
 use Perlito5::Java::Runtime;
 use Perlito5::Java::Lib;
 
+# Perlito5::Perl5 might be useful for debugging
+use Perlito5::Perl5::Emitter;
+use Perlito5::Perl5::PrettyPrinter;
+use Perlito5::Perl5::Runtime;
+
+
 $Perlito5::JAVA_EVAL = 1;
 $Perlito5::CODE_TOO_LARGE = 1;  # work around java code size limitation
 
