5630c68e251c4b6932a32e41f4fad5943fc7bb35
Flavio S. Glock <fglock@gmail.com>
2017-10-06 15:36:16 +0200

    Perlito5 - misc - "binary file" test

--- misc/Perl5/create_binary_file.pl
+++ misc/Perl5/create_binary_file.pl
@@ -1,9 +1,18 @@
 
-# create a "binary" file;
+# create "binary" files with different encodings;
+#
 # test with:
 # $ hexdump file.bin 
+# $ hexdump utf8.bin 
 
 open OUT, ">", "file.bin"
+    or die $!;
+for (0..255) {
+    print OUT chr($_);
+}
+close OUT;
+
+open OUT, "> :encoding(UTF-8)", "utf8.bin"
     or die $!;
 for (0..255) {
     print OUT chr($_);
