e8347d0c9a959275acf7083b76d34102b4add5b1
Flavio S. Glock <fglock@gmail.com>
2017-11-08 21:31:22 +0100

    Perlito5 - java - tr() preprocessor wip

--- src5/lib/Perlito5/Regex.pm
+++ src5/lib/Perlito5/Regex.pm
@@ -10,8 +10,24 @@ sub expand_character_range {
     while (@range) {
 
         if ( @range >= 2 && $range[0] eq '\\' ) {
+            if ( $range[1] ge '0' && $range[1] le '3' ) {
+                # start octal   \200
+                if ( $range[2] ge '0' && $range[2] le '7' ) {
+                    if ( $range[3] ge '0' && $range[3] le '7' ) {
+                        push @out, chr( $range[1] * 64 + $range[2] * 8 + $range[3] );
+                        shift @range for 1..4;
+                        next;
+                    }
+                    push @out, chr( $range[1] * 8 + $range[2] );
+                    shift @range for 1..3;
+                    next;
+                }
+                push @out, chr( $range[1] );
+                shift @range for 1..2;
+                next;
+            }
 
-            # TODO - \x68 \N{U+20} \200 \n
+            # TODO - \x68 \N{U+20} \n
 
             push @out, $range[1];
             shift @range for 1..2;
@@ -19,6 +35,9 @@ sub expand_character_range {
         }
 
         if ( @range >= 3 && $range[1] eq '-' ) {
+
+            # TODO - \200-\210
+
             if (  $range[0] ge 'A' && $range[0] le 'Z'
                && $range[1] eq '-'
                && $range[2] ge 'A' && $range[2] le 'Z'
