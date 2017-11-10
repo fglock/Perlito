2e6ffd07f32a5ae0ee93d82a8e1b6d04e3a8a2e3
Flavio S. Glock <fglock@gmail.com>
2017-11-10 12:53:12 +0100

    Perlito5 - java - caller() docs

--- src5/lib/Perlito5/Java/CORE.pm
+++ src5/lib/Perlito5/Java/CORE.pm
@@ -2120,6 +2120,9 @@ EOT
                 // TODO - this code doesn't account for inner-subs - it might match an outer sub instead
                 // TODO - this code doesn't account for package name changes inside a sub
                 // TODO - this code skips anonymous subroutines
+                // TODO - the "perlSubName" resoltution is ambiguous,
+                //        because a sub can be exported to several namespaces:
+                //        Exporter::import and MyClass::import can point to the same Java code
                 // this loop does a symbol table scan - PlV.cvar
               SCAN_SUBNAME:
                 for (PlObject perlSubName : (PlArray)PlCORE.keys(PlCx.LIST, PlV.cvar)) {
