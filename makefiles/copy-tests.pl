67c1be9bb3c6d544f57819dbbd6e16cd7b31794c
Flavio S. Glock <fglock@gmail.com>
2017-11-15 13:54:59 +0100

    Perlito5 - tests - workaround "cant unlink" in travis

--- makefiles/copy-tests.pl
+++ makefiles/copy-tests.pl
@@ -2,26 +2,61 @@
 
 use strict;
 use Data::Dumper;
+use File::Find;
+use File::Copy;
 
 my $usage = "Usage: makefiles/copy-tests.pl t5/Test-summary-report-5jar.txt t5-jar";
 
 my $test_report = shift || die $usage;
-my $out_dir = shift || die $usage;
+my $out_dir     = shift || die $usage;
 
 open my $f, "<", $test_report;
 
 my @files = <$f>;
 
-# warn Dumper(\@files);
-warn "running from directory: ", `pwd`;
+# warn "running from directory: ", `pwd`;
 
+my %bad_file;
 for my $line (@files) {
-    if ($line =~ m{^t5/(\S+)}) {
-        my $bad_file = $out_dir . '/' . $1;
-        # warn "$bad_file\n";
-        unlink $bad_file
-            or (system("rm", $bad_file) == 0)
-            or warn "Can't unlink $bad_file: $!";
+    if ( $line =~ m{^(t5/\S+)} ) {
+        $bad_file{$1} = 1;
+
+        # warn "bad file $1\n";
     }
+}
+
+my @files;
+my @dir;
+finddepth(
+    {
+        no_chdir => 1,
+        wanted   => sub {
+            # warn "name $File::Find::name ", ( -d $File::Find::name ? "dir" : "not" ), "\n";
+            if   ( -d $File::Find::name ) { push @dir,   $File::Find::name }
+            else                          { push @files, $File::Find::name }
+        },
+    },
+    "t5"
+);
+
+for my $name ( sort { length($a) <=> length($b) } @dir) {
+    # warn "$name is directory\n";
+    $name =~ s/^t5/$out_dir/;
+    if (! -d $name) {
+        mkdir $name
+            or warn "Can't mkdir $name\n";
+    }
+}
+for my $name (@files) {
+    # warn "file $name\n";
+    if ( $bad_file{$name} ) {
+        # warn "bad file $name\n";
+        next;
+    }
+    my $out = $name;
+    $out =~ s/^t5/$out_dir/;
+    # warn "copy file $name to $out\n";
+    copy( $name, $out )
+      or warn "Copy $name to $out failed: $!";
 }
 
