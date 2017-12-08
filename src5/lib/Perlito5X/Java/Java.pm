88b3573555d2149c6e2a7af7c555a2d05afaa5f9
Flavio S. Glock <fglock@gmail.com>
2017-12-08 21:07:36 +0100

    Perlito5 - java - Java extensions wip

--- src5/lib/Perlito5X/Java/Java.pm
+++ src5/lib/Perlito5X/Java/Java.pm
@@ -7,9 +7,30 @@ sub inline {
 
 sub type ($$) {
     # from Nashorn: var HashMap = Java.type("java.util.HashMap")
+    my ( $class, $type ) = @_;
+    my $array = "";
+    while ( substr( $type, -2, 2 ) eq "[]" ) {
+        # int[][]  => [[I
+        # Object[] => [Ljava.lang.Object;
+        $type = substr( $type, 0, -2 );
+        $array .= "[";
+    }
+    if    ( $type eq "boolean" ) { $type = $array . "Z"; }
+    elsif ( $type eq "byte" )    { $type = $array . "B"; }
+    elsif ( $type eq "char" )    { $type = $array . "C"; }
+    elsif ( $type eq "double" )  { $type = $array . "D"; }
+    elsif ( $type eq "float" )   { $type = $array . "F"; }
+    elsif ( $type eq "int" )     { $type = $array . "I"; }
+    elsif ( $type eq "long" )    { $type = $array . "J"; }
+    elsif ( $type eq "short" )   { $type = $array . "S"; }
+    elsif ( $array )             { $type = $array . "L" . $type . ";" }
+    _type($type);
+}
+
+sub _type ($) {
     eval {
         Java::inline q{
-            Class.forName( List__.aget(1).toString() )
+            Class.forName( List__.aget(0).toString() )
         }
     };
 }
