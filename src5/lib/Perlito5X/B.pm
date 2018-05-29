53c1d4a1fb58787f094f32d9199d9333fe1238c5
Flavio S. Glock <fglock@gmail.com>
2018-05-29 14:56:17 +0200

    Perlito5 - add B::perlstring()

--- src5/lib/Perlito5X/B.pm
+++ src5/lib/Perlito5X/B.pm
@@ -1,4 +1,19 @@
 package B;
 
+use Data::Dumper;  # escape_string
+
+sub import {
+    my $class = shift;
+    my $caller = caller(0);
+
+    if (grep { $_ eq 'perlstring' } @_) {
+        *{"${caller}::perlstring"} = \&perlstring;
+    }
+}
+
+sub perlstring {
+    return Data::Dumper::escape_string(@_);
+}
+
 1;
 
