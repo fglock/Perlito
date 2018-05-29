dac1425257a6e35a688d974d7997e290bac004ae
Flavio S. Glock <fglock@gmail.com>
2018-05-29 13:38:24 +0200

    Perlito5 - Class::XSAccessor - add getters, setters

--- src5/lib/Perlito5X/Class/XSAccessor.pm
+++ src5/lib/Perlito5X/Class/XSAccessor.pm
@@ -3,14 +3,44 @@ package Class::XSAccessor;
 
 sub import {
     my $class = shift;
-    my %args = @_;
+    my %args;
+    if ( ref($_[0]) eq "HASH" ) {
+        %args = %{$_[0]};
+    }
+    else {
+        %args = @_;
+    }
 
     my $caller = caller(0);
+
+    for my $param ( "getters", "setters", "accessors" ) {
+        if (ref($args{$param}) eq 'ARRAY') {
+            $args{$param} = { map { $_ => $_ } @{$args{$param}} };
+        }
+    }
+
+    my $getters = delete $args{getters};
+    if ($getters) {
+        for my $name (keys %$getters) {
+            my $key = $getters->{$name};
+            *{"${caller}::${name}"} = sub {
+                $_[0]->{$key}
+            };
+        }
+    }
+
+    my $setters = delete $args{setters};
+    if ($setters) {
+        for my $name (keys %$setters) {
+            my $key = $setters->{$name};
+            *{"${caller}::${name}"} = sub {
+                $_[0]->{$key} = $_[1]
+            };
+        }
+    }
+
     my $accessors = delete $args{accessors};
     if ($accessors) {
-        if (ref($accessors) eq 'ARRAY') {
-            $accessors = { map { $_ => $_ } @$accessors };
-        }
         for my $name (keys %$accessors) {
             my $key = $accessors->{$name};
             # print STDERR "generate accessor for $caller $class $name => $key\n";
