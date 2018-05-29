e50dbda0bfae875d7dbc2ca053e8bbc1dcc843be
Flavio S. Glock <fglock@gmail.com>
2018-05-29 11:26:43 +0200

    Perlito - dist - add PERLITO_ROOT in cpan-v6/make-v6.sh

--- cpan-v6/make-v6.sh
+++ cpan-v6/make-v6.sh
@@ -1,20 +1,22 @@
 [ -d cpan-v6 ] && cd cpan-v6
 
+PERLITO_ROOT=..
+
 rm -rf lib
 rm -rf t
 
 touch META.yml
 
 mkdir t
-cp ../t6/*.t t/
+cp $PERLITO_ROOT/t6/*.t t/
 perl -pi -e 's/use v6;/use v6-perlito;/' t/*.t
 
 rm -rf t/bugs
-cp -r ../lib5 lib
+cp -r $PERLITO_ROOT/lib5 lib
 rm -rf lib/Perlito5
 
-cp ../src6/lib/Perlito6/Python/Prelude.pm lib/Perlito6/Python/Prelude.pm
-cp ../src6/lib/Perlito6/Ruby/Runtime.rb   lib/Perlito6/Ruby/Runtime.rb
+cp $PERLITO_ROOT/src6/lib/Perlito6/Python/Prelude.pm lib/Perlito6/Python/Prelude.pm
+cp $PERLITO_ROOT/src6/lib/Perlito6/Ruby/Runtime.rb   lib/Perlito6/Ruby/Runtime.rb
 
 perldoc -otext v6.pm > README
 
@@ -24,7 +26,7 @@ cp perlito.pm lib/v6/
 
 rm -rf scripts
 mkdir scripts
-cp ../perlito6.pl scripts/perlito6
+cp $PERLITO_ROOT/perlito6.pl scripts/perlito6
 
 perl Makefile.PL
 
