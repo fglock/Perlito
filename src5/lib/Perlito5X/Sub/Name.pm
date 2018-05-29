a5f799aa226410d984733b42f6842f8bc6b6e7c2
Flavio S. Glock <fglock@gmail.com>
2018-05-29 13:49:13 +0200

    Perlito5 - Sub::Name - add export

--- src5/lib/Perlito5X/Sub/Name.pm
+++ src5/lib/Perlito5X/Sub/Name.pm
@@ -1,5 +1,12 @@
 package Sub::Name;
 
+sub import {
+    my $pkg     = shift;
+    my $callpkg = caller(0);
+    *{ $callpkg . "::subname" } = \&subname;
+    return;
+}
+
 sub subname {
     my ($name, $sub) = @_;
     # TODO
