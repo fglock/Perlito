5b9e79e4d1dc637078f3698276d2fded275725e6
Flavio S. Glock <fglock@gmail.com>
2018-08-21 10:32:51 +0200

    Perlito5 - CORE - add warnings.pm

--- src5/lib/Perlito5X/warnings/register.pm
+++ src5/lib/Perlito5X/warnings/register.pm
@@ -3,6 +3,17 @@ package warnings::register;
 our $VERSION = '1.04';
 require warnings;
 
+# left here as cruft in case other users were using this undocumented routine
+# -- rjbs, 2010-09-08
+sub mkMask
+{
+    my ($bit) = @_;
+    my $mask = "";
+
+    vec($mask, $bit, 1) = 1;
+    return $mask;
+}
+
 sub import
 {
     shift;
