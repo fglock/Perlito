01f99a75002957a4ffbee1741562ffb2a30a5d7b
Flavio S. Glock <fglock@gmail.com>
2018-12-19 16:36:26 +0100

    Perlito5 - add POSIX::ceil, floor

--- src5/lib/Perlito5X/POSIX.pm
+++ src5/lib/Perlito5X/POSIX.pm
@@ -3,11 +3,23 @@ use strict;
 
 use Exporter qw(import);
 our @EXPORT_OK = qw(
+    floor
+    ceil
     strftime
 );
 
+sub floor {
+    # from: Math::Utils
+    ($_[0] < 0 and int($_[0]) != $_[0])? int($_[0] - 1): int($_[0])
+}
+
+sub ceil {
+    # from: Math::Utils
+    ($_[0] > 0 and int($_[0]) != $_[0])? int($_[0] + 1): int($_[0])
+}
+
 sub strftime {
-    return "strftime not yet implemented";  # TODO See: https://metacpan.org/pod/POSIX::strftime::GNU::PP
+    return "strftime not yet implemented";  # TODO See: DateTime.pm
 }
 
 1;
