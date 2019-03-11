a7b30a178f53e62e295386a72c57872747b49923
Flavio S. Glock <fglock@gmail.com>
2019-03-11 20:17:45 +0100

    Perlito5 - updated ChangeLog, code cleanup

--- src5/lib/Perlito5/Grammar/Precedence.pm
+++ src5/lib/Perlito5/Grammar/Precedence.pm
@@ -282,9 +282,6 @@ sub precedence_parse {
     while ((defined($token)) && ($token->[0] ne 'end')) {
         my $token_is_term = ($token->[0] eq 'term') || ($token->[0] eq 'postfix_or_term') || ($token->[0] eq 'postfix');
 
-        # if ($token->[1] eq "=>" && @$num_stack >= 1 && ref($num_stack->[-1]) eq 'ARRAY' && @{ $num_stack->[-1] } >= 1) {
-        #     $num_stack->[-1][1] = Perlito5::AST::Lookup->autoquote($num_stack->[-1][1]);
-        # }
         if (($token->[1] eq ',' || $token->[1] eq '=>') && !@$num_stack) {
             #  comma without a preceding term
             Perlito5::Compiler::error( "Syntax error" );
