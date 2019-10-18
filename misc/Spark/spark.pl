47ef73a563f6db371ed21591170ef68aeefceb5b
Flavio S. Glock <fglock@gmail.com>
2019-10-18 22:54:32 +0200

    Perlito5 - java - Spark example - wip

--- misc/Spark/spark.pl
+++ misc/Spark/spark.pl
@@ -3,7 +3,7 @@ package JavaRDD { import => "org.apache.spark.api.java.JavaRDD" }
 package JavaSparkContext { import => "org.apache.spark.api.java.JavaSparkContext" }
 package SparkSession { import => "org.apache.spark.sql.SparkSession" }
 package Integer { import => "java.lang.Integer" }
-package List::Integer { import => "java.util.ArrayList<java.lang.Integer>" }
+package List { import => "java.util.ArrayList<PlObject>" }
 
 use strict;
 use warnings;
@@ -22,10 +22,9 @@ my $n = 100000 * $slices;
 
 print "slices: $slices; n: $n\n";
 
-my List::Integer $list = List::Integer->new();
+my List $list = List->new();
 for ( 0 .. $n - 1 ) {
-    my Integer $i = int($_);
-    $list->add($i);
+    $list->add($_);
 }
 
 my $dataSet = $jsc->parallelize($list, $slices);
@@ -35,13 +34,13 @@ my $count;
 
 # TODO
 # $count = $dataSet->map(
-#         sub {
+#         sub ($) {
 #               my $x = rand(2) - 1;
 #               my $y = rand(2) - 1;
 #               return ($x * $x + $y * $y <= 1) ? 1 : 0;
 #         }
 #     )->reduce(
-#         sub { $_[0] + $_[1] }
+#         sub ($$) { $_[0] + $_[1] }
 #     );
 
 print "Pi is roughly ", 4.0 * $count / $n, "\n";
