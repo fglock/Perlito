c77e43432ff645f55cf0547dc680a76e6f672233
Flavio S. Glock <fglock@gmail.com>
2019-10-18 22:47:21 +0200

    Perlito5 - java - Spark example - wip

--- misc/Spark/spark.pl
+++ misc/Spark/spark.pl
@@ -2,8 +2,6 @@
 package JavaRDD { import => "org.apache.spark.api.java.JavaRDD" }
 package JavaSparkContext { import => "org.apache.spark.api.java.JavaSparkContext" }
 package SparkSession { import => "org.apache.spark.sql.SparkSession" }
-package Java::ArrayList { import => "java.util.ArrayList" }
-package Java::List { import => "java.util.List" }
 package Integer { import => "java.lang.Integer" }
 package List::Integer { import => "java.util.ArrayList<java.lang.Integer>" }
 
@@ -32,19 +30,18 @@ for ( 0 .. $n - 1 ) {
 
 my $dataSet = $jsc->parallelize($list, $slices);
 
-sub pi_func {
-      my $x = rand(2) - 1;
-      my $y = rand(2) - 1;
-      return ($x * $x + $y * $y <= 1) ? 1 : 0;
-}
 
 my $count;
 
 # TODO
 # $count = $dataSet->map(
-#         ...
+#         sub {
+#               my $x = rand(2) - 1;
+#               my $y = rand(2) - 1;
+#               return ($x * $x + $y * $y <= 1) ? 1 : 0;
+#         }
 #     )->reduce(
-#         ...
+#         sub { $_[0] + $_[1] }
 #     );
 
 print "Pi is roughly ", 4.0 * $count / $n, "\n";
