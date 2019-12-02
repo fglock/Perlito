a3dba730bb2d63355e7d44ccc714f1def1ca1566
Flavio S. Glock <fglock@gmail.com>
2019-12-02 21:06:46 +0100

    Perlito5 - java - Serialization experiment

---
+++ misc/Java_serialize/Serialize.java
@@ -0,0 +1,41 @@
+// $ cp misc/Java/Serialize.java .
+// $ javac -cp .:perlito5.jar Serialize.java
+// $ java -cp .:perlito5.jar Serialize
+
+import javax.script.*;
+import java.io.*;
+
+public class Serialize {
+    public static void main(String[] args) throws Exception {
+
+        ScriptEngineManager factory = new ScriptEngineManager();
+        ScriptEngine engine = factory.getEngineByName("Perl5");
+
+        org.perlito.Perlito5.PlObject o = (org.perlito.Perlito5.PlObject) engine.eval("  456 ");
+        System.out.println("result: " + o);
+
+        System.out.println( pickle(o) );
+        System.out.println( unpickle( pickle(o), org.perlito.Perlito5.PlObject.class ) );
+    }
+
+
+    private static <T extends Serializable> byte[] pickle(T obj) 
+           throws IOException 
+    {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(baos);
+        oos.writeObject(obj);
+        oos.close();
+        return baos.toByteArray();
+    }
+    
+    private static <T extends Serializable> T unpickle(byte[] b, Class<T> cl)
+           throws IOException, ClassNotFoundException 
+    {
+        ByteArrayInputStream bais = new ByteArrayInputStream(b);
+        ObjectInputStream ois = new ObjectInputStream(bais);
+        Object o = ois.readObject();
+        return cl.cast(o);
+    }
+
+}
