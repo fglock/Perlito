40882187efe50134614c204be468a7f51b2471d8
Flavio S. Glock <fglock@gmail.com>
2024-07-22 15:19:55 +0200

    Perlito5 - misc/Java-Asm-Interpreter/MethodExecutor works

--- misc/Java-Asm-Interpreter/MethodExecutor/MethodExecutor.java
+++ misc/Java-Asm-Interpreter/MethodExecutor/MethodExecutor.java
@@ -29,62 +29,29 @@ public class MethodExecutor {
         Object[] trimmedArgs = new Object[argIndex];
         System.arraycopy(args, 0, trimmedArgs, 0, argIndex);
 
-        MethodResult result = invokeMethod(target, methodName, trimmedArgs);
-        return result.discard ? new MethodResult(null, true) : result;
-    }
+        // System.out.println("Invoking method: " + methodName + " on class: " + target.getClass().getName());
+        // System.out.println("Arguments: ");
+        // for (Object arg : trimmedArgs) {
+        //     System.out.println("  - " + (arg == null ? "null" : arg.getClass().getName()));
+        // }
 
-    private static MethodResult invokeMethod(Object target, String methodName, Object[] args) throws Exception {
-        System.out.println("Invoking method: " + methodName + " on class: " + target.getClass().getName());
-        System.out.println("Arguments: ");
-        for (Object arg : args) {
-            System.out.println("  - " + (arg == null ? "null" : arg.getClass().getName()));
-        }
-
-        if (target instanceof PrintStream && methodName.equals("println")) {
-            return invokePrintStreamMethod((PrintStream) target, args);
-        }
-
-        Class<?>[] argTypes = new Class[args.length];
-        for (int i = 0; i < args.length; i++) {
-            argTypes[i] = (args[i] == null) ? Object.class : getPrimitiveClass(args[i].getClass());
+        Class<?>[] argTypes = new Class[trimmedArgs.length];
+        for (int i = 0; i < trimmedArgs.length; i++) {
+            argTypes[i] = (trimmedArgs[i] == null) ? Object.class : getPrimitiveClass(trimmedArgs[i].getClass());
         }
 
         Method method = target.getClass().getMethod(methodName, argTypes);
-        System.out.println("Resolved method: " + method.getName() + " with parameter types: ");
-        for (Class<?> paramType : method.getParameterTypes()) {
-            System.out.println("  - " + paramType.getName());
-        }
+        // System.out.println("Resolved method: " + method.getName() + " with parameter types: ");
+        // for (Class<?> paramType : method.getParameterTypes()) {
+        //     System.out.println("  - " + paramType.getName());
+        // }
 
-        Object result = method.invoke(target, args);
+        Object result = method.invoke(target, trimmedArgs);
 
         // If the method returns void, set discard flag to true
         boolean discard = method.getReturnType() == void.class;
 
         return new MethodResult(result, discard);
-    }
-
-    private static MethodResult invokePrintStreamMethod(PrintStream target, Object[] args) throws Exception {
-        Class<?> printStreamClass = target.getClass();
-        Method printlnMethod;
-
-        if (args.length == 0) {
-            printlnMethod = printStreamClass.getDeclaredMethod("println");
-        } else if (args.length == 1) {
-            Class<?> argType = (args[0] == null) ? Object.class : getPrimitiveClass(args[0].getClass());
-            printlnMethod = printStreamClass.getDeclaredMethod("println", argType);
-        } else {
-            throw new IllegalArgumentException("PrintStream.println method accepts at most one argument");
-        }
-
-        System.out.println("Resolved PrintStream method: " + printlnMethod.getName() + " with parameter types: ");
-        for (Class<?> paramType : printlnMethod.getParameterTypes()) {
-            System.out.println("  - " + paramType.getName());
-        }
-
-        printlnMethod.invoke(target, args);
-
-        // PrintStream.println is a void method, so set discard flag to true
-        return new MethodResult(null, true);
     }
 
     private static Class<?> getPrimitiveClass(Class<?> clazz) {
