cf07bb60f814ee933b415a5862966aa0b91fd5b2
fglock <fglock@gmail.com>
2024-07-23 22:17:29 +0200

    Perlito5 - misc/Java-Asm-Interpreter/MethodExecutorAsm cleanup

--- misc/Java-Asm-Interpreter/MethodExecutorAsm/MethodResult.java
+++
@@ -1,9 +0,0 @@
-public class MethodResult {
-    public final Object result;
-    public final boolean discard;
-
-    public MethodResult(Object result, boolean discard) {
-        this.result = result;
-        this.discard = discard;
-    }
-}
