21f20d0c84d803fdfb048974ff2d26f6e3a37c9f
Flavio S. Glock <fglock@gmail.com>
2024-07-30 17:20:27 +0200

    Perlito5 - misc/Java-Asm-Interpreter/MethodExecutorAsm fix ->

---
+++ misc/Java-Asm-Interpreter/MethodExecutorAsm/AnonSubNode.java
@@ -0,0 +1,37 @@
+/**
+ * The AnonSubNode class represents a node in the abstract syntax tree (AST) that holds an anonymous sub.
+ * The parts of the node are: "block".
+ * This class implements the Node interface, allowing it to be visited by a Visitor.
+ *
+ * <p>The AnonSubNode class is used to encapsulate "sub" in the AST, providing
+ * a way to store and manipulate "sub" and their parts within the tree structure.</p>
+ */
+public class AnonSubNode implements Node {
+    /**
+     * The block of the subroutine
+     */
+    public final Node block;
+
+    /**
+     * Constructs a new AnonSubNode with the specified parts.
+     *
+     * @param block the block of the subroutine
+     */
+    public AnonSubNode(Node block) {
+        this.block = block;
+    }
+
+    /**
+     * Accepts a visitor that performs some operation on this node.
+     * This method is part of the Visitor design pattern, which allows
+     * for defining new operations on the AST nodes without changing
+     * the node classes.
+     *
+     * @param visitor the visitor that will perform the operation on this node
+     */
+    @Override
+    public void accept(Visitor visitor) throws Exception {
+        visitor.visit(this);
+    }
+}
+
